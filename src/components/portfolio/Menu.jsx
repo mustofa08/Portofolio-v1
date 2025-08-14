36384e046bc38ad7c0df0eb180b7e44c321634bc
mustofa08 <tofa354kendal@gmail.com>
2025-08-14 20:24:45 +0700

    upade foto porto

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,6 +1,6 @@
-import Work1 from "../../assets/work-1.svg";
+import Work1 from "../../assets/ecommerce.png";
 import Work2 from "../../assets/Hangman.png";
-import Work3 from "../../assets/work-3.svg";
+import Work3 from "../../assets/CurrencyD.png";
 // import Work4 from "../../assets/work-4.svg";
 // import Work5 from "../../assets/work-5.svg";
 // import Work6 from "../../assets/work-6.svg";
